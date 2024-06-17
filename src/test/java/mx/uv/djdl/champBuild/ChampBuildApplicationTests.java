5a04fa6f861daa5b00d38e532a799dcf41cf2378
Diego Dominguez <Diegodgz.182@gmail.com>
2024-06-17 14:27:11 -0600

    Se modificó toca la estructura para utilizar maven en vez de gradle

---
+++ src/test/java/mx/uv/djdl/champBuild/ChampBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.djdl.champBuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
